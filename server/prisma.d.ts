6437901e235091161d6e50c70a9233b310465733
Blinko <blinkospace@gmail.com>
2025-09-24 14:52:01 +0800

    chore: support sqlite

--- server/prisma.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Instantiates a single instance PrismaClient and save it on the global object.
- * @see https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
- */
-import { PrismaClient } from '@prisma/client';
-export declare const prisma: PrismaClient;
--- server/prisma.ts
+++ server/prisma.ts
@@ -3,6 +3,23 @@
  * @see https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
  */
 import { PrismaClient } from '@prisma/client';
+import { PrismaBetterSQLite3 } from '@prisma/adapter-better-sqlite3';
+
+
+
+// Determine database type and configuration
+const DATABASE_TYPE = process.env.DATABASE_TYPE || 'postgres';
+const isPostgres = DATABASE_TYPE === 'postgres';
+const isSQLite = DATABASE_TYPE === 'sqlite';
+
+// Set up database URL based on type
+let databaseUrl = process.env.DATABASE_URL;
+let adapter: PrismaBetterSQLite3
+if (isSQLite && !databaseUrl) {
+  adapter = new PrismaBetterSQLite3({
+    url:  "file:../server/.blinko/sqlite/sqlite.db"
+  });
+}
 
 const prismaGlobal = globalThis as typeof globalThis & {
   prisma?: PrismaClient;
@@ -10,11 +27,12 @@ const prismaGlobal = globalThis as typeof globalThis & {
 export const prisma: PrismaClient =
   prismaGlobal.prisma ??
   new PrismaClient({
+    ...(adapter && { adapter }),
     log:
-    process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+      process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
     datasources: {
       db: {
-        url: process.env.DATABASE_URL
+        url: databaseUrl
       }
     }
   });
